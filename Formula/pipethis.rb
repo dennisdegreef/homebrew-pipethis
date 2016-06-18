4ddc171bce9a1029c288206637d353a05c573942
Dennis de Greef <github@link0.net>
2016-06-18 21:51:11 +0200

    Add version, since autodetection breaks on this URL

--- Formula/pipethis.rb
+++ Formula/pipethis.rb
@@ -3,6 +3,7 @@ class Pipethis < Formula
   homepage "https://github.com/ellotheth/pipethis"
   url "https://github.com/ellotheth/pipethis/releases/download/v0.5.1/pipethis-v0.5.1-darwin-amd64.tar.bz2"
   sha256 "aa7244d8d42be7d6e2f5e8b483a8f6f2b1a14516bfc8c9317518eab5f7f5f15c"
+  version "0.5.1"
 
   def install
     bin.install('pipethis')

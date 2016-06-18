a532994ed453d8e5760cd2cb310ed70f1c637deb
Dennis de Greef <github@link0.net>
2016-06-18 21:28:45 +0200

    Fix URL and use Darwin version instead of Linux

--- Formula/pipethis.rb
+++ Formula/pipethis.rb
@@ -1,7 +1,7 @@
 class Pipethis < Formula
   desc "Replace your `curl | sh` installers"
   homepage "https://github.com/ellotheth/pipethis"
-  url "https://github.com/ellotheth/pipethis/releases/download/v0.5/pipethis-0.5-linux-amd64.tar.bz2"
+  url "https://github.com/ellotheth/pipethis/releases/download/v0.5.1/pipethis-v0.5.1-darwin-amd64.tar.bz2"
   sha256 "aa7244d8d42be7d6e2f5e8b483a8f6f2b1a14516bfc8c9317518eab5f7f5f15c"
 
   def install
